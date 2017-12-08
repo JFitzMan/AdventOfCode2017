8e7e5080baeefb4579cd4de2515393a683390c62
Jordan Fitzpatrick <fitzpatrickj1@email.arizona.edu>
2017-12-08 16:51:25 -0700

    Day 7 part 2. What are trees again? Program can calculate the weight of each tower. Now needs to find the one that is wrong.

--- Day7.py
+++ Day7.py
@@ -1,5 +1,47 @@
 import re
 from collections import defaultdict
+
+class Node:
+    name = ''
+    children = []
+    parent = ''
+    weight = 0
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+        self.children = []
+        self.parent = ''
+
+    def addChild(self, child):
+        self.children.append(child)
+
+    def getName(self):
+        return self.name
+
+    def getChildren(self):
+        return self.children
+
+    def getWeight(self):
+        return self.weight
+
+    def getParent(self):
+        return self.parent
+
+    def setParent(self, parent):
+        self.parent = parent
+
+    def weightOfTower(self, nodes):
+        sum = int(self.weight)
+        print(self.weight)
+        for c in self.children:
+            print(c)
+            sum += int(nodes[c].weightOfTower(nodes))
+        return sum
+
+
+
+
 def day7():
     regex = re.compile(r'^(\w+) \((\d+)\)(?: -> ([\w, ]+))?')
     parent = defaultdict(bool)
@@ -17,4 +59,37 @@ def day7():
             if parentStatus is False:
                 print(name)
 
-day7()
+def day7_pt2():
+    regex = re.compile(r'^(\w+) \((\d+)\)(?: -> ([\w, ]+))?')
+    nodes = {}
+    node = None
+    with open('day7.txt', 'r') as f:
+        # Populate dictionary with node names, weights, and children
+        for line in f:
+            m = regex.match(line)
+            # Group 1 is name, group 2 is weight
+            node = Node(m.group(1), m.group(2))
+            nodes[m.group(1)] = node
+            if m.group(3) is not None:
+                for proc in m.group(3).split(','):
+                    node.addChild(proc.strip())
+
+        # Populate parents
+        for name, node in nodes.items():
+            children = node.getChildren()
+            if len(children) > 0:
+                for c in children:
+                    if c in nodes:
+                        nodes[c].setParent(name)
+
+
+        for name, node in nodes.items():
+            print('name: '+node.getName())
+            print('children: '+str(node.getChildren()))
+            print('parent: '+node.getParent())
+            #print('weight: '+node.weightOfTower(nodes))
+            print()
+        print(nodes['ugml'].weightOfTower(nodes))
+
+#day7()
+day7_pt2()
